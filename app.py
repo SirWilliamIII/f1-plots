ee2c3de038e69615972df24aa2f1b3a53c1e2c9e
SirWilliamIII <willcarpenterwastaken@gmail.com>
2025-06-18 17:57:56 -0500

    removed races, centered form

--- app.py
+++ app.py
@@ -915,3 +915,7 @@ def metrics():
     """Prometheus metrics endpoint"""
     return generate_latest(), 200, {"Content-Type": CONTENT_TYPE_LATEST}
 
+
+
+if  __name__ == "__main__":
+    app.run(host="0.0.0.0",debug=True)
